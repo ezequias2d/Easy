f88f6d943d79a3607fab84a5a1937c73d5892f0e
ezequias2d <ezequiasmoises@gmail.com>
2020-08-01 21:10:25 -0300

    Add ICompression coments.

--- EasyBitmap/Compression/ICompression.cs
+++ EasyBitmap/Compression/ICompression.cs
@@ -5,10 +5,23 @@ using System.Text;
 
 namespace Easy.Compression
 {
+    /// <summary>
+    /// A compression algorithm
+    /// </summary>
     public interface ICompression
     {
+        /// <summary>
+        /// Compress an input stream to output stream.
+        /// </summary>
+        /// <param name="input">Input stream.</param>
+        /// <param name="output">Output stream.</param>
         void Compress(Stream input, Stream output);
 
+        /// <summary>
+        /// Decompress an input stream to output stream.
+        /// </summary>
+        /// <param name="input">Input stream.</param>
+        /// <param name="output">Output stream</param>
         void Decompress(Stream input, Stream output);
     }
 }
